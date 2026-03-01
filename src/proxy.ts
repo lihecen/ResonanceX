38f1fbfc59f6b95465eb7b84afa2e69b05f36bdc
Lihecen <2837842053@qq.com>
2026-03-01 22:59:25 +0800

    feat: project setup and authentication & database

---
+++ src/proxy.ts
@@ -0,0 +1,31 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isOrgSelectionRoute = createRouteMatcher(["/org-organization(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, orgId } = await auth();
+  if (isPublicRoute(req)) {
+    return NextResponse.next();
+  }
+  if (!userId) {
+    await auth.protect();
+  }
+
+  if (isOrgSelectionRoute(req)) {
+    return NextResponse.next();
+  }
+
+  if (userId && !orgId) {
+    const orgSelection = new URL("/org-organization", req.url);
+    return NextResponse.redirect(orgSelection);
+  }
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
